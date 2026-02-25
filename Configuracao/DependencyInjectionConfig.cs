3c9cccfa575160e3ffd30f3ebfc83734746f5d2c
Gustavo Rodrigues <gustavorodrigues955@gmail.com>
2026-02-24 21:57:28 -0300

    feat: Preparando worker

---
+++ Configuracao/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSAgroNotificacao.Configuracao
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            #region Services/Repository   
+
+            #endregion
+
+            return services;
+        }
+    }
+}
--- Worker.cs
+++ Worker.cs
@@ -17,7 +17,7 @@ namespace MSAgroNotificacao
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
             }
         }
     }
